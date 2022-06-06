33155e3bd5380519a3385b0793612f7024665bef
mikebender <mikebender@deephaven.io>
2022-06-06 13:53:13 -0400

    Update the filter to only look for matplotlib figures

--- matplotlib/src/DashboardPlugin.tsx
+++ matplotlib/src/DashboardPlugin.tsx
@@ -9,6 +9,8 @@ import { VariableDefinition } from '@deephaven/jsapi-shim';
 import Log from '@deephaven/log';
 import MatPlotLibPanel from './MatPlotLibPanel';
 
+export const VARIABLE_TYPE = 'matplotlib.figure.Figure';
+
 const log = Log.module('@deephaven/js-plugin-matplotlib.DashboardPlugin');
 
 export const DashboardPlugin = ({
@@ -29,8 +31,8 @@ export const DashboardPlugin = ({
       widget: VariableDefinition;
     }) => {
       const { id: widgetId, name, type } = widget;
-      if (type === dh.VariableType.TABLE || type === dh.VariableType.FIGURE) {
-        // Just ignore table and figure types - only want interesting other types
+      if ((type as string) !== VARIABLE_TYPE) {
+        // Only want to listen for matplotlib panels
         return;
       }
       log.info('Panel opened of type', type);
